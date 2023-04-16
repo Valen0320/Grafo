8dbf70bf9c7f4e06104d25f26c15228c470e2044
Valentina Idarraga Henao <vidarraga94279@gmail.com>
2023-04-16 13:51:50 -0500

    Commit

--- src/main/java/co/edu/umanizales/msgrafo/model/City.java
+++ src/main/java/co/edu/umanizales/msgrafo/model/City.java
@@ -2,11 +2,15 @@ package co.edu.umanizales.msgrafo.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "cities")
 @AllArgsConstructor
 @Data
 
 public class City {
+    @Id
     private String code;
     private String name;
     private float latitude;
